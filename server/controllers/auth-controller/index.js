9c1952c9a53ddb70ebb6bc60e2a7e206f1fb95c6
AnishBahadurKarki <np03cs4a220011@heraldcollege.edu.np>
2025-04-07 07:23:31 +0545

    register user completed

---
+++ server/controllers/auth-controller/index.js
@@ -0,0 +1,26 @@
+const User = require("../../models/User");
+const registerUser = async (req, res) => {
+  const { userName, userEmail, password, role } = req.body;
+  console.log("triggrered")
+  const existingUser = await User.findOne({
+    $or: [{ userEmail }, { userName }],
+  });
+  if (existingUser) {
+    return res.status(400).json({
+      success: false,
+      message: "User already exists",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    userName,
+    userEmail,
+    role,
+    password: hashedPassword,
+  });
+  await newUser.save();
+  return res.status(201).json({
+    success: true,
+    message: "user registered successfull",
+  });
+};
