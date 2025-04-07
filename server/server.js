937eb425dab547523b82f12f3621aeb080718750
AnishBahadurKarki <np03cs4a220011@heraldcollege.edu.np>
2025-04-07 07:01:31 +0545

    initial setup done

--- server/server.js
+++ server/server.js
@@ -1,38 +1,34 @@
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const router = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-
-app.use(
-  cors({
-    origin: 
-    '*',
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-type", "Authorization"],
+});
 
 app.use(express.json());
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("mongodb is connected"))
-  .catch((e) => console.log(e));
 
+//database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongoDB is connected"))
+  .catch((e) => console.log(e));
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(500).json({
     success: false,
-    message: "Something went wrong",
+    message: "something went wrong",
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is now running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
